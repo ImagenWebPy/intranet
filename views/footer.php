19b52f034528ecf209c548f85335df759a504928
Raul Ramirez <raul.ramirez@imagenwebhq.com>
2017-05-25 21:15:30 -0400

    Se agrega el nuevo template

---
+++ views/footer.php
@@ -0,0 +1,23 @@
+<script src="<?= URL; ?>public/assets/js/vendor/jquery-1.10.2.min.js"></script>
+<script src="<?= URL; ?>public/assets/js/vendor/jquery-ui-1.10.4.custom.min.js"></script>
+<script src="<?= URL; ?>public/assets/js/vendor/jquery.touchSwipe.min.js"></script>
+
+<script src="<?= URL; ?>public/assets/js/bootstrap/carousel.js"></script>
+<script src="<?= URL; ?>public/assets/js/bootstrap/tab.js"></script>
+<script src="<?= URL; ?>public/assets/js/bootstrap/collapse.js"></script>
+<script src="<?= URL; ?>public/assets/js/bootstrap/transition.js"></script>
+
+      <!--[if IE]><script type="text/javascript" src="<?= URL; ?>public/assets/js/vendor/excanvas.js"></script><![endif]-->
+<script src="<?= URL; ?>public/assets/js/vendor/jquery.knob.min.js"></script>
+<script src="<?= URL; ?>public/assets/js/vendor/fastclick.min.js"></script>
+<script src="<?= URL; ?>public/assets/js/vendor/jquery.stellar.min.js"></script>
+
+<script src="<?= URL; ?>public/assets/js/vendor/jquery.mousewheel.js"></script>
+<script src="<?= URL; ?>public/assets/js/vendor/perfect-scrollbar.min.js"></script>
+
+<script src="<?= URL; ?>public/assets/js/vendor/isotope.pkgd.js"></script>
+
+<script src="<?= URL; ?>public/assets/js/plugins.js"></script>
+<script src="<?= URL; ?>public/assets/js/main.js"></script>
+</body>
+</html>
---
+++ views/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+
+        <title><?= SITE_TITLE; ?></title>
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link href='http://fonts.googleapis.com/css?family=Oxygen:400,700' rel='stylesheet' type='text/css'>
+
+        <link rel="stylesheet" href="<?= URL; ?>public/assets/css/style.css">
+        <script src="<?= URL; ?>public/assets/js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body class="menu-bar-opened menu-bar-ontop black-body">
